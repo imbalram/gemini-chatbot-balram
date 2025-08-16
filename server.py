c7b6fff2c319df28879b507e85f6420d083929e1
Balram thakur <56107244+imbalram@users.noreply.github.com>
2025-08-16 20:50:17 +0530

    Update server.py

--- server.py
+++ server.py
@@ -62,5 +62,5 @@ def chat():
     return jsonify({"reply": bot_reply})
 
 if __name__ == "__main__":
-    print("🚀 Server running at http://localhost:5000")
+    print("🚀 Server running at http://sakshibalram:5000")
     app.run(host="0.0.0.0", port=5000, debug=True)
